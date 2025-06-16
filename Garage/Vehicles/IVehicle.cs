86eb36950122e635615a24d83449c25627d597b0
wpplng <lina.wappling@gmail.com>
2025-06-16 13:58:50 +0200

    Implemented Vehicle and IVehicle

---
+++ Garage/Vehicles/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Vehicles
+{
+    internal interface IVehicle
+    {
+        string RegistrationNumber { get; }
+        string Color { get; }
+        int NumberOfWheels { get; }
+    }
+}
---
+++ Garage/Vehicles/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Vehicles
+{
+    internal abstract class Vehicle : IVehicle
+    {
+        public string RegistrationNumber { get; }
+        public string Color { get; }
+        public int NumberOfWheels { get; }
+
+        protected Vehicle(string regNr, string color, int numberOfWheels)
+        {
+            RegistrationNumber = regNr.ToUpper();
+            Color = color;
+            NumberOfWheels = numberOfWheels;
+        }
+
+        public override string ToString()
+        {
+            return $"RegNr: {RegistrationNumber}, Color: {Color}, Wheels: {NumberOfWheels}";
+        }
+    }
+}
