79137729a87a702fdf87133561263804d8bf0a8d
wpplng <lina.wappling@gmail.com>
2025-06-17 11:07:36 +0200

    Handler class with field for garage and constructor

---
+++ Garage/Handler.cs
@@ -0,0 +1,43 @@
+﻿using Garage.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    internal class Handler : IHandler
+    {
+        private IGarage<IVehicle> _garage;
+
+        public Handler(IGarage<IVehicle> garage)
+        {
+            _garage = garage ?? throw new ArgumentNullException(nameof(garage), "Garage cannot be null");
+        }
+        public bool AddVehicle(IVehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVehicle? FindVehicle(string regNr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IVehicle> GetAllVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, int> GetVehicleTypeCounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveVehicle(string regNr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
