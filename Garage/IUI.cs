f4fa2d5d2c622a8edee7c9d584910dceb9a28b2a
wpplng <lina.wappling@gmail.com>
2025-06-17 11:41:55 +0200

    UI Interface

---
+++ Garage/IUI.cs
@@ -0,0 +1,17 @@
+﻿using Garage.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    internal interface IUI
+    {
+        void Start();
+        string GetInput(string prompt);
+        void ShowMessage(string message);
+        void ShowVehicles(IEnumerable<IVehicle> vehicles);
+    }
+}
